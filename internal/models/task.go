eff6f8d6bfab89a3ce555954da1dcd9cbfa064da
b.kushnir <bohdan.kushnir@kyivstar.net>
2025-08-18 11:33:43 +0300

    🔨 refactor(app): 17 update data type for Customers

--- internal/models/task.go
+++ internal/models/task.go
@@ -1,17 +1,20 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	pb "github.com/UnknownOlympus/olympus-protos/gen/go/scraper/olympus"
+)
 
 type Task struct {
-	ID            int       `json:"id"`
-	Type          string    `json:"type"`
-	CreatedAt     time.Time `json:"createdAt"`
-	ClosedAt      time.Time `json:"closedAt"`
-	Description   string    `json:"description"`
-	Address       string    `json:"address"`
-	CustomerName  string    `json:"customerName"`
-	CustomerLogin string    `json:"customerLogin"`
-	Comments      []string  `json:"comments"`
-	Executors     []string  `json:"executors"`
-	IsClosed      bool      `json:"is_closed"`
+	ID          int            `json:"id"`
+	Type        string         `json:"type"`
+	CreatedAt   time.Time      `json:"createdAt"`
+	ClosedAt    time.Time      `json:"closedAt"`
+	Description string         `json:"description"`
+	Address     string         `json:"address"`
+	Customers   []*pb.Customer `json:"customers"`
+	Comments    []string       `json:"comments"`
+	Executors   []string       `json:"executors"`
+	IsClosed    bool           `json:"is_closed"`
 }
